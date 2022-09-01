98b096d07ebde785980dd82df8b5ef525dd01964
rivaldy <frizgt@gmail.com>
2022-09-01 17:26:46 +0800

    :cherries: :label: change StatefulWidget to StatelessWidget HomePage

--- lib/features/home/presentation/pages/index/page.dart
+++ lib/features/home/presentation/pages/index/page.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'sections/sections.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
